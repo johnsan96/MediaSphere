cb13f6e6cb9af18748990390105b7a9371a649c4
john <john@getcoding.de>
2024-03-27 17:34:31 +0100

    init

---
+++ backendMediaSphere/src/main/java/com/john/backendMediaSphere/BackendMediaSphereApplication.java
@@ -0,0 +1,13 @@
+package com.john.backendMediaSphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMediaSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMediaSphereApplication.class, args);
+	}
+
+}
---
+++ backendMediaSphere/src/test/java/com/john/backendMediaSphere/BackendMediaSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.john.backendMediaSphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMediaSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
